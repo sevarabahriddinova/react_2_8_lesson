7dfd8a36286c32a06eb2c0e7cd60d6a03769034e
sevarabahriddinova <sevaraxonbahriddinova@gmail.com>
2024-09-13 15:10:46 +0500

    link

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import { useGetProductsQuery } from '../redux/api';
+import Login from './Login';
+import Link from 'antd/es/typography/Link';
  
 
 const Home = () => {
 
   const { data: products, error, isLoading } = useGetProductsQuery();
 
-  if (isLoading) return <p>Loading...</p>;
+  if (isLoading) return <p className='text-center mt-[300px] font-bold text-blue-800 text-[26px]'>Loading...</p>;
   if (error) return <p>Error fetching products</p>;
 
   return (
+
+    
    <div className='w-[1200px]  m-auto bg-gray-300 p-[30px] rounded-md'>
      <div className="w-full flex flex-col gap-4 m-auto">
-      <h1 className="text-center text-blue-800 font-bold text-[28px] p-6"> You are welcome</h1>
+      <div className='flex gap-4 items-center justify-center '>
+        <Link to={"/login"} className=' border rounded-md p-2 bg-white px-4 text-center text-blue-800 font-bold text-[28px] '>Login</Link>
+      <h1 className="border rounded-md p-2.5 bg-white px-4 text-center text-blue-800 font-bold text-[28px] "> You are welcome</h1>
+      </div>
       <ul className="w-full flex flex-wrap gap-6 ">
         {products.map((product) => (
-          <li key={product.id} className="w-[200px] bg-red-200 shadow-2xl rounded-lg flex flex-col gap-6 justify-center  items-center">
+          <li key={product.id} className="w-[200px] bg-red-200 shadow-2xl rounded-lg flex flex-col p-6 gap-6 justify-center  items-center">
             <h2 className="product-name">{product.title}</h2>
             <p className="product-price">${product.price}</p>
             <img src={product.images} alt={product.name} className="product-image" />
